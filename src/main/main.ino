c0e0204803d223d6aceb8579af1587bbf66b5ac7
Nolan Adams <nolan.adams@protonmail.com>
2017-08-29 18:26:16 -0400

    fixed bug with title error on lcd

--- src/main/main.ino
+++ src/main/main.ino
@@ -3,9 +3,10 @@
 #include <SoftwareSerial.h>
 SoftwareSerial serial(2, 3); //RX, TX
 /*
-Due to the fact that the MCU running the display is slower than the one we are programming, we need to
-include lots of delays for them to catch up to eachother, so they don't trip over commands and we don't
-end up with a corrupted image on the display.
+Due to the fact that the MCU running the display is slower than the one we are
+programming, we need to include lots of delays for them to catch up to eachother,
+so they don't trip over commands and we don't end up with a corrupted image on
+the display.
 -Nolan Adams
 This code is released under an MIT License
 */
@@ -30,6 +31,7 @@ void setup()
   setY(8); // ^
   delay(50); //stability delay
   serial.print("Sensor Test Bench"); //title at the top of the screen
+  delay(50);
   setX(25);
   setY(23);
   serial.print("Input");
@@ -39,7 +41,7 @@ void setup()
 
   delay(200); //replaces smile for stability's sake
   printXColumn();
-  delay(100);
+  delay(100); //delay before loop starts
 }
 
 void loop()
@@ -48,10 +50,11 @@ void loop()
   printYColumn();
   delay(1000);
 
-  //drawBox(76, 33, 154, 80, 0);
+  //drawBox(76, 33, 154, 80, 0); //to show where the erased area is
 
   //delay(100);
   setHome();
   eraseBlock(76, 33, 154, 80);
+  //clear the area where the values are displayed to remove residual digits if value gains or loses a digit
 
 }
